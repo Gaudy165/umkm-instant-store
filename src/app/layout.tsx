64cd7af6e63e4076d069d79d58d1be5a5132a68f
Gaudy165 <adityagaudy31@gmail.com>
2026-03-12 13:31:42 +0700

    Fitur: Menginisialisasi CSS global dengan Tailwind, Shadcn, dan tema kustom, serta mengatur tata letak root dengan metadata, font, AuthProvider, dan Navbar.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Storezy - UMKM Instant Store Builder",
@@ -27,9 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased pt-16`}
-      >
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
+      </head>
+      <body className="antialiased pt-16">
         <AuthProvider>
           <Navbar />
           {children}
